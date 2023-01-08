a7d8585141bd423a2cbc0663870dcf9b84fc5ff7
Mathias Rav <m@git.strova.dk>
2023-01-08 16:17:17 +0100

    Fix read_one_keystroke() with timeout

--- miniscreen/consoleio.py
+++ miniscreen/consoleio.py
@@ -7,6 +7,9 @@ def read_one_keystroke(
     timeout: float | None, extra_timeout: float | None = 0.1, fd=0
 ) -> str:
     "Read exactly one Linux console keystroke from stdin"
+    first_char = wait_stdin_read1(timeout, fd=fd)
+    if not first_char:
+        return ""
     return parse_one_keystroke(
-        wait_stdin_read1(timeout, fd=fd), lambda: wait_stdin_read1(extra_timeout, fd=fd)
+        first_char, lambda: wait_stdin_read1(extra_timeout, fd=fd)
     )
